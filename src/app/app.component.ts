01aa305e953d611a245929a6762e947b8cfa9c87
Sameer Siddiqui <sameersiddiqui@quickenloans.com>
2020-04-30 12:18:34 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Task } from './interfaces/task';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Lab163';
+  title = 'Lab 16.3: Angular Todo';
+
+  userInput:string;
+
+  tasks:Task[] = [
+    {taskTitle: "Play video games", completed: false},
+    {taskTitle: "Work on lab", completed: false},
+    {taskTitle: "Take a nap", completed: true},
+    {taskTitle: "procrastinate working on lab", completed: true},
+    {taskTitle: "Eat Dinner", completed: false},
+    {taskTitle: "Buy cat food", completed: false}
+  ]
+
+  addTask = function(){
+    this.tasks.push({taskTitle:this.userInput, completed: false});
+  }
+
+  completeTask = function(task:Task) {
+    task.completed = true;
+  }
+
+  deleteTask = function(task:Task, index){
+    this.tasks.splice(index, 1);
+  }
+
 }
